ea30604fb4330c54027a541ad8a35d656c65b72a
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-07-20 17:39:13 +0900

    changed to take const reference of Type

--- type.h
+++ type.h
@@ -24,7 +24,7 @@ class Type {
   bool is_ptr();
 
   friend std::shared_ptr<Type> create_type(Kind, int);
-  friend bool operator==(Type, Type);
+  friend bool operator==(const Type &, const Type &);
 
   // for pointer
   std::shared_ptr<Type> m_next;
@@ -46,7 +46,7 @@ std::shared_ptr<Type> add_type(std::shared_ptr<Type> type, Kind kind, int size);
 std::shared_ptr<Type> add_int(std::shared_ptr<Type> type);
 std::shared_ptr<Type> add_ptr(std::shared_ptr<Type> type);
 
-bool operator==(Type, Type);
-bool operator!=(Type, Type);
+bool operator==(const Type &, const Type &);
+bool operator!=(const Type &, const Type &);
 
 }  // namespace type
