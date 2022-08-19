1cb4e1c52fa1b7e3175fc1e163af303291db347b
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-08-19 12:56:52 +0900

    feat: rc_gc: Changed to save reference count in additional region.

--- rc_gc.c
+++ rc_gc.c
@@ -4,69 +4,69 @@
 
 // #define POC
 
-int* rc_malloc(int size) {
-  int* tmp = (int*)malloc(size);
-  set_count(&tmp, 0);
-  return tmp;
+void* rc_malloc(int size) {
+  void* tmp = (void*)malloc(size + 4);
+  set_count(tmp, 0);
+  return make_defined(tmp);
 }
 
-void rc_free(int* ptr) {
-  delete_count(&ptr);
-  free(ptr);
-}
+void rc_free(void* ptr) { free(delete_flag(ptr)); }
 
-void rc_delete(int** ptr) {
-  if (get_count(*ptr) == 0) return;
+void rc_delete(void* ptr) {
+  if (!is_defined(ptr)) return;
   dec_count(ptr);
-  if (get_count(*ptr) == 0) {
+  if (get_count(ptr) == 0) {
     // TODO: Need to free children.
     // How to identify children?
-    rc_free(*ptr);
+    rc_free(ptr);
   }
 }
 
-void rc_update(int** dst, int** src) {
-  rc_delete(dst);
+void rc_update(void** dst, void* src) {
+  rc_delete(*dst);
   inc_count(src);
+  *dst = src;
 }
 
 // Helper functions
 
-short get_count(int* ptr) {
+short get_count(void* ptr) {
   // Upper 16bit is a counter.
-  return (unsigned long)ptr >> 48;
+  return *((int*)delete_flag(ptr));
 }
 
-void set_count(int** ptr, short count) {
-  // Create mask
-  unsigned long next_mask = (unsigned long)count << 48;
-
-  // Clear upper 16bit.
-  unsigned long addr = (unsigned long)*ptr;
-  addr = (addr << 16) >> 16;
-
-  // Update
-  *ptr = (int*)(addr | next_mask);
+void set_count(void* ptr, int c) {
+  int* ref_count = (int*)delete_flag(ptr);
+  *ref_count = c;
 }
 
-void dec_count(int** ptr) { set_count(ptr, get_count(*ptr) - 1); }
-void inc_count(int** ptr) { set_count(ptr, get_count(*ptr) + 1); }
+void dec_count(void* ptr) { set_count(ptr, get_count(ptr) - 1); }
+void inc_count(void* ptr) { set_count(ptr, get_count(ptr) + 1); }
 
-void delete_count(int** ptr) {
-  if ((unsigned long)*ptr & 0x0000800000000000) {
-    *ptr = (int*)((unsigned long)*ptr | 0xffff000000000000);
-  } else {
-    *ptr = (int*)((unsigned long)*ptr & 0x0000ffffffffffff);
-  }
+void* make_defined(void* ptr) {
+  unsigned long addr = (unsigned long)ptr;
+  addr = addr & 0x0000ffffffffffff;
+  addr = addr + 0x0001000000000000;
+  return (void*)addr;
+}
+
+int is_defined(void* ptr) {
+  unsigned long addr = (unsigned long)ptr;
+  return (addr & 0xffff000000000000) == 0x0001000000000000;
+}
+
+void* delete_flag(void* ptr) {
+  long addr = (long)ptr;
+  addr = addr << 16;
+  addr = addr >> 16;
+  return (void*)addr;
 }
 
 #ifdef POC
 int main(int argc, char* argv[]) {
-  int *mptr, *tmp;
+  void *mptr, *tmp;
   for (;;) {
-    tmp = mptr;
-    mptr = (int*)rc_malloc(1024);
-    rc_update(&tmp, &mptr);
+    rc_update(&mptr, rc_malloc(1024));
   }
   return 0;
 }
